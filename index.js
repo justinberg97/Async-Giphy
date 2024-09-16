26984c9ac5c349d9c954da7a05f9e1fbf1e5f4a5
Justin Berg <justin.berg1@verizon.com>
2024-09-16 14:10:02 -0500

    created .env file and obtained api url

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const endpoint = https://api.giphy.com/v1/gifs/random?api_key=xgddlfISy7qJ9YgQxkvXn8VeqHszYdRE&tag=&rating=pg-13
+
+
