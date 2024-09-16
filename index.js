03ee5c6d51dd4d8c567236bfb5d5fe5c1850fe77
Justin Berg <justin.berg1@verizon.com>
2024-09-16 14:40:54 -0500

    working on rendering my image

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ require('dotenv').config();
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
 
-const endpoint = https://api.giphy.com/v1/gifs/random?api_key=xgddlfISy7qJ9YgQxkvXn8VeqHszYdRE&tag=&rating=pg-13
+const endpoint = 'https://api.giphy.com/v1/gifs/random?api_key=' + process.env.API_KEY + '&tag=&rating=pg-13'
 
 
+async function getImage(query) {
+    try {
+        const response = await fetch(`${endpoint}${query}`);
+        const data = await response.json;
+        const url = data.data.images.original.url;
+        console.log(url)
+    } catch (error) {
+        console.error('Error fetching the gif:' , error)
+    }
+}
+
+getImage('dogs');
